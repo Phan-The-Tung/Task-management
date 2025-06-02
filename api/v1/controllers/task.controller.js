41f514c567fecdd376c0e0e9ae550282683d5a3c
Phan-The-Tung <phantung17072004@gmail.com>
2025-06-02 10:48:29 +0700

    Lấy ds task đúng theo user

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -4,6 +4,10 @@ const paginationHelpers = require("../../../helpers/pagination");
 // [GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
     const find = {
+        $or: [
+          { createdBy: req.user.id},
+          { listUser: req.user.id}
+        ],
         deleted: false
     }
 
