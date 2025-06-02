51df46927dd832eef2d2a836ed9dc15ff5e79a4d
Phan-The-Tung <phantung17072004@gmail.com>
2025-06-02 08:18:16 +0700

    Quên mật khẩu

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -1,7 +1,10 @@
 const User = require("../models/user.model");
+const ForgotPassword = require("../models/forgot-password.model");
+const generateHelper = require("../../../helpers/generate");
+const sendMailHelper = require("../../../helpers/sendMail");
 const md5 = require("md5");
 
-// [GET] /api/v1/tasks
+// [POST] /api/v1/user/register
 module.exports.register = async (req, res) => {
     req.body.password = md5(req.body.password);
 
@@ -37,3 +40,80 @@ module.exports.register = async (req, res) => {
     }
 
 };
+
+// [POST] /api/v1/user/login
+module.exports.login = async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+  
+    const user = await User.findOne({
+      email: email,
+      deleted: false
+    });
+  
+    if (!user) {
+      res.json({
+        code: 400,
+        message: "Email không tồn tại!"
+      });
+      return;
+    }
+  
+    if (md5(password) !== user.password) { // md5(password) là giả định, hàm md5 cần được import hoặc định nghĩa
+      res.json({
+        code: 400,
+        message: "Sai mật khẩu!" // Hoặc một thông báo khác như "Mật khẩu không đúng!"
+      });
+      return;
+    }
+  
+    const token = user.token;
+    res.cookie("token", token);
+  
+    res.json({
+      code: 200,
+      message: "Đăng nhập thành công!",
+      token: token
+    });
+  };
+
+// [POST] /api/v1/users/password/forgot
+module.exports.forgotPassword = async (req, res) => {
+    const email = req.body.email;
+  
+    const user = await User.findOne({
+      email: email,
+      deleted: false,
+    });
+  
+    if (!user) {
+      res.json({
+        code: 400,
+        message: "Email không tồn tại!",
+      });
+      return;
+    }
+  
+    const otp = generateHelper.generateRandomNumber(8); // Giả định generateHelper được import/định nghĩa
+    const timeExpire = 5; // Tính bằng phút
+
+    const objectForgotPassword = {
+        email: email,
+        otp: otp,
+        expireAt: Date.now()   // Giả định timeExpire là thời gian hết hạn tính bằng phút
+      };
+      
+      const forgotPassword = new ForgotPassword(objectForgotPassword);
+      await forgotPassword.save();
+
+      // Nếu tồn tại email thì gửi mã OTP qua email
+
+     const subject = "Mã OTP xác minh lấy lại mật khẩu";
+     const html = `Mã OTP để lấy lại mật khẩu là <b style="color: red;">${otp}</b>. Thời hạn sử dụng là 3 phút.`;
+     sendMailHelper.sendMail(email, subject, html);
+      
+      res.json({
+        code: 200,
+        message: "Dã gửi mã OTP qua email"
+      });
+  };
--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -5,4 +5,8 @@ const controllers = require("../controllers/user.controller") ;
 
 router.post("/register", controllers.register);
 
+router.post("/login", controllers.login);
+
+router.post("/password/forgot", controllers.forgotPassword);
+
 module.exports = router;
